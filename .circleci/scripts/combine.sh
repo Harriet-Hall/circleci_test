68f05baa504bccc339ec0985eafa839e2306771e
Harriet Hall <harriet.hall@madetech.com>
2024-10-06 14:03:39 +0100

    add pre hook which run script combine.sh

---
+++ .circleci/scripts/combine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd .circleci
+
+
+
+for f in common.yml config1.yml config2.yml config3.yml workflows.yml; do (cat "${f}"; echo) >> continue_config.yml; done
+
+# cat common.yml >> continue_config.yml
+# cat config1.yml >> continue_config.yml
+# cat config2.yml >> continue_config.yml
+# cat config3.yml >> continue_config.yml
+# cat workflows.yml >> continue_config.yml
+
