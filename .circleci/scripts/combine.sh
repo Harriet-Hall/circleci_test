0a0e57c9e19ce29f7e69a24e0b51466778376d36
Harriet Hall <harriet.hall@madetech.com>
2024-10-07 09:30:01 +0100

    wip

--- .circleci/scripts/combine.sh
+++ .circleci/scripts/combine.sh
@@ -5,7 +5,7 @@ cd .circleci
 > continue_config.yml
 
 
-for f in common.yml /configs/config1.yml /configs/config2.yml /configs/config3.yml workflows.yml; do (cat "${f}"; echo) >> continue_config.yml; done
+for f in common.yml configs/config1.yml configs/config2.yml configs/config3.yml workflows.yml; do (cat "${f}"; echo) >> continue_config.yml; done
 
 # cat common.yml >> continue_config.yml
 # cat config1.yml >> continue_config.yml
