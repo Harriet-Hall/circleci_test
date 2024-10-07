11a5aca20ba265f40c4e34bd767739a333be7432
Harriet Hall <harriet.hall@madetech.com>
2024-10-07 09:34:21 +0100

    update configs directory name

--- .circleci/scripts/combine.sh
+++ .circleci/scripts/combine.sh
@@ -4,7 +4,6 @@ cd .circleci
 
 > continue_config.yml
 
-
 for f in common.yml configs/config1.yml configs/config2.yml configs/config3.yml workflows.yml; do (cat "${f}"; echo) >> continue_config.yml; done
 
 # cat common.yml >> continue_config.yml
