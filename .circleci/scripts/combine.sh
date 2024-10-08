d3e0543d3fcb6d9b82f6fcc6e816ebbb9d2ccbd8
Harriet Hall <harriet.hall@madetech.com>
2024-10-08 18:07:14 +0100

    refactor file paths
    Signed-off-by: Harriet Hall <harriet.hall@madetech.com>

--- .circleci/scripts/combine.sh
+++ .circleci/scripts/combine.sh
@@ -4,4 +4,4 @@ cd .circleci
 
 > continue_config.yml
 
-for f in common.yml configs/config1.yml configs/config2.yml configs/config3.yml workflows.yml; do (cat "${f}"; echo) >> continue_config.yml; done
+for f in common.yml configs/*.yml workflows.yml; do (cat "${f}"; echo) >> continue_config.yml; done
